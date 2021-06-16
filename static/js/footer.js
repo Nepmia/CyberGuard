e441ac7863b01dc46b9b57b218e4777e4926e6be
Nepmia <64558289+Nepmia@users.noreply.github.com>
2021-06-16 13:34:04 +0200

    made footer placer for responsive pos

---
+++ static/js/footer.js
@@ -0,0 +1,24 @@
+function footerPlacer() { 
+    var page = $(".page-content").height();  // Content height (since we use a container for the page instead of body)
+    var footer = $(".foot").height(); // Footer height 
+    var footer_pos = page - footer; // Calculate the difference between footer and page so we can move the footer at the bottom of the page
+    $(".foot").css("top", footer_pos + "px"); // gives the footer an absolute position at the bottom of the page (also page has min-heigh : 100vh so the footer is always at bottom)
+}
+function footerHidder(status){
+    if (status == "on"){
+        $(".foot").addClass("hidden");
+    } else {
+        $(".foot").removeClass("hidden");
+    }
+}
+
+$(window).resize(() =>{ // Listen for page resize so we can re-calculate the footer pos
+    footerHidder("on");
+    footerPlacer();
+    setTimeout(() =>{ // Just a timeout to be sure the window has correctly maximized 
+      footerPlacer();
+      setTimeout(() => {
+        footerHidder();
+      },500)
+    }, 300)
+})
