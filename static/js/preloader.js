a70981485dc2f51be57c188c2cd3f4e80be4cb18
Nepmia <64558289+Nepmia@users.noreply.github.com>
2021-06-16 13:36:21 +0200

    made prealoader functs

---
+++ static/js/preloader.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    $("#preloader").removeAttr("loop");
+    $("video").on("ended", function(){
+        footerPlacer();
+        $(".preloader-table").addClass("hidden");
+        $("body").removeClass("antiscroll");
+        $(".eContainer").each(function(){
+            if ($(this).hasClass("eContainer-Scroller")) {
+                // pass
+            } else {
+                var rClass = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+                $(this).addClass(rClass);
+                eContainerPopScroll(rClass);
+            }
+        });
+    });
+  }
