a04b782796cd471bb1e978ebf65010a0fdfba830
Nepmia <64558289+Nepmia@users.noreply.github.com>
2021-06-16 13:38:04 +0200

    created sidenav functs

---
+++ static/js/sidenav.js
@@ -0,0 +1,30 @@
+$(document).ready(() => { // Basic function that enable and disable the sidenav on btn clicks
+    $(".navbar-ham").click(() =>{
+        sideNav("on")
+    });
+
+    $(".sidenav-closeBtn").click(() =>{
+        sideNav("off")
+    });
+
+    $(".sidenav-noInt").click(() =>{
+        sideNav("off")
+    });
+});
+
+function sideNav (status){
+    if (status == "on") {
+        $(".sidenav").addClass("sideOn").removeClass("sideOff");
+        $(".sidenav-noInt").removeClass("none");
+        setTimeout(() =>{
+            $(".sidenav-noInt").removeClass("hidden").addClass("visible");
+        },10);
+    }
+    else {
+        $(".sidenav").addClass("sideOff").removeClass("sideOn");
+        $(".sidenav-noInt").removeClass("visible").addClass("hidden");
+        setTimeout(() => {
+            $(".sidenav-noInt").addClass("none");
+        }, 300)
+    }
+}
