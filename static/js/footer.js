07b53ca4b1ef3364e40ce48a9e998e2a4ff631a4
Nepmia <64558289+Nepmia@users.noreply.github.com>
2021-06-17 20:41:07 +0200

    Fixed footer not sticking at bottom of page

--- static/js/footer.js
+++ static/js/footer.js
@@ -2,7 +2,7 @@ function footerPlacer() {
     var page = $(".page-content").height();  // Content height (since we use a container for the page instead of body)
     var footer = $(".foot").height(); // Footer height 
     var footer_pos = page - footer; // Calculate the difference between footer and page so we can move the footer at the bottom of the page
-    $(".foot").css("top", footer_pos + "px"); // gives the footer an absolute position at the bottom of the page (also page has min-heigh : 100vh so the footer is always at bottom)
+    $(".foot").css("top", footer_pos + 65 + "px"); // gives the footer an absolute position at the bottom of the page (also page has min-heigh : 100vh so the footer is always at bottom)
 }
 function footerHidder(status){
     if (status == "on"){
